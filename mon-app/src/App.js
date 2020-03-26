89738101aea6d431f51caec640e632b4b4bd370c
Duchemin <tonyduchemin@MacBook-Pro-de-tony.local>
2020-03-26 16:31:26 +0100

    Création Composants Pure, utilisation Props et State

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {PureCompo, PureCompoProps} from './components/PureCompo';
+import StateCompo from './components/StateCompo';
 
 function App() {
   return (
@@ -10,6 +12,19 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <StateCompo  label={"Tony"} />
+        <PureCompo/>
+        <PureCompoProps label={"Tony"} age={"29"} />
+
+
+
+
+
+
+
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
